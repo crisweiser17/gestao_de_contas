34f3648e092235a8b47f0b9dbf825997d11bd592
crisweiser17 <crisweiser17@users.noreply.github.com>
2025-11-14 08:45:20 -0300

    Sessão de usuário configurada para 24h (gc_maxlifetime e cookie lifetime)

--- config/config.php
+++ config/config.php
@@ -104,6 +104,8 @@ date_default_timezone_set('America/Sao_Paulo');
 
 // Configurações de sessão
 if (session_status() === PHP_SESSION_NONE) {
+    ini_set('session.gc_maxlifetime', 86400);
+    session_set_cookie_params(86400, '/', '', (ENVIRONMENT === 'production'), true);
     session_start();
 }
 
